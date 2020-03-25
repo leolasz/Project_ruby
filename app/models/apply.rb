a699eac585218c7ee5467daa1e0f31774326a174
leonardo souza <leo.aus88@gmail.com>
2020-03-25 12:05:49 +1100

    update files

--- app/models/apply.rb
+++ app/models/apply.rb
@@ -1,4 +1,8 @@
 class Apply < ApplicationRecord
-
+    validates :name, presence: true, length: { maximum: 50 }
+    validates :age, presence: true, length: { maximum: 3 }
+    validates :position, presence: true, length: { maximum: 50 }
+    validates :phone, presence: true, length: { maximum: 50 }
+    validates :email, presence: true, length: { maximum: 50 }
     
 end
--- app/models/create_job.rb
+++ app/models/create_job.rb
@@ -1,2 +1,8 @@
 class CreateJob < ApplicationRecord
+    validates :name, presence: true, length: { maximum: 50 }
+    validates :position, presence: true, length: { maximum: 50 }
+    validates :position, presence: true, length: { maximum: 50 }
+    validates :annual_salary, presence: true, length: { maximum: 50 }
+    validates :contract_time, presence: true, length: { maximum: 50 }
+
 end
